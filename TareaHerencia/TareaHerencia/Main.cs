28adc3cdfd35fb4a3f3a6b21a250ece3e89ca12f
Ricardo Tapia <RickTC@localhost.localdomain>
2013-10-22 11:04:30 -0500

    inicio

---
+++ TareaHerencia/TareaHerencia/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace TareaHerencia
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			
+		}
+	}
+}
---
+++ TareaHerencia/TareaHerencia/Persona.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TareaHerencia
+{
+	public class Persona
+	{
+		
+		public string nombre;
+		public string apellido;
+		public int id;
+		public int telefono;
+		
+		
+		public Persona ()
+		{
+		}
+	}
+}
+
