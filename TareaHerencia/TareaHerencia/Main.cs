e1a2ee374e618de832825c72ad6db93ef0c0cc59
Ricardo Tapia <RickTC@localhost.localdomain>
2013-10-22 12:41:32 -0500

    main listo.

--- TareaHerencia/TareaHerencia/Main.cs
+++ TareaHerencia/TareaHerencia/Main.cs
@@ -6,11 +6,23 @@ namespace TareaHerencia
 	{
 		public static void Main (string[] args)
 		{
-			Alumno alumno = new Alumno( 209514538,"Ricardo","Tapia",3314864958);
-		//	Persona persona = new Persona();
+			Alumno alumno = new Alumno( 209,"Ricardo","Tapia",33148);
+			//Persona persona = new Persona(236,"Porfirio","Cervantes",32569);
 			
-			alumno.datos();
-			alumno.muestra();
+			
+			
+			Console.WriteLine("CLASS ALUMNO");
+			Console.WriteLine("ID: "+ alumno.id);
+			Console.WriteLine("Nombre: "+ alumno.nombre);
+			Console.WriteLine("Apellido: "+alumno.apellido);
+			Console.WriteLine("Telefono: "+alumno.telefono);
+			/*Console.WriteLine("CLASS PERSONA");
+			Console.WriteLine("ID: "+ persona.id);
+			Console.WriteLine("Nombre: "+ persona.nombre);
+			Console.WriteLine("Apellido: "+ persona.apellido);
+			Console.WriteLine("Telefono: "+persona.telefono);*/
+			
+			
 		}
 	}
 }
